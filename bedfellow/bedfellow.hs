e557405ce59fcb666be24ac7310dfa7e508d126d
Thomas Levine <_@thomaslevine.com>
2014-07-10 16:25:03 -0400

    -

--- bedfellow/bedfellow.hs
+++ bedfellow/bedfellow.hs
@@ -1,7 +1,18 @@
 data Person  = Person String
 data Company = Company String
+type P = Person
+type C = Company
 
 type Employment = (Person, Company)
 type Bedfellowship = (Person, Person)
+
+-- (---) :: Person -> Company -> Employment
+-- (---) _ _ = "aoe"
+
+a :: Person -> Company -> Employment
+a = undefined
+
+a :: Person -> Person -> Employment
+a = undefined
 
 main = do putStrLn "3"
